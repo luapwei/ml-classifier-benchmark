a1947818bb38f645a8efcb8623b6b708a4f48fcc
Paul Weiß <paule.weisss@gmail.com>
2026-03-10 14:32:29 +0100

    Added Loggin

--- src/adapter.py
+++ src/adapter.py
@@ -1,4 +1,4 @@
-from src.interfaces import ClassifierStrategy
+from src.interfaces import ClassifierStrategy, log_method
 
 
 class SklearnAdapter(ClassifierStrategy):
@@ -6,8 +6,10 @@ class SklearnAdapter(ClassifierStrategy):
     def __init__(self, sklearn_model):
         self.model = sklearn_model
 
+    @log_method
     def fit(self, X, y):
         self.model.fit(X, y)
 
+    @log_method
     def predict(self, X):
         return self.model.predict(X)
